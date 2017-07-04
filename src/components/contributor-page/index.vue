e09edc6d14c601c65292a31fd18437208f03427e
Arnvvd <pinot.arnaud@gmail.com>
2017-07-05 01:24:18 +0200

    [Feat:Github] - add github-cards component on vue

--- src/components/contributor-page/index.vue
+++ src/components/contributor-page/index.vue
@@ -1,12 +1,17 @@
 <template>
     <div id="contributor">
         <h1>You are a {{contributorType}}</h1>
+        <github-cards></github-cards>
     </div>
 </template>
 
 <script>
+    import githubCardsList from './githubCardsList.vue'
     export default {
         name: 'contributor',
+        components: {
+            'github-cards': githubCardsList
+        },
         computed: {
             contributorType () {
                 return this.$route.params.contributor
