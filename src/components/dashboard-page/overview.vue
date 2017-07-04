afd8630627916fbab72b52a60d6e1ced573de6c6
renanbronchart <renan.bronchart@gmail.com>
2017-07-05 01:24:39 +0200

    [Feat:Dashboard] Add nav, overview and navside components

---
+++ src/components/dashboard-page/overview.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <section>
+      <h2>Overview</h2>
+      <Card :title='Presets' :img=''>
+        <p slot='description'>
+          Description presets
+        </p>
+      </Card>
+      <Card :title='Presets' :img=''>
+        <p slot='description'>
+          Description Articles
+        </p>
+      </Card>
+    </section>
+  </div>
+</template>
+
+<script>
+  import Card from './card.vue'
+
+  export default {
+    name: 'Overview',
+    data () {
+      return {
+      }
+    },
+    components: {
+      Card
+    }
+  }
+</script>
+
+<style>
+
+</style>
