5813ccef3d583a009fca2542997f30318d0f632c
renanbronchart <renan.bronchart@gmail.com>
2017-07-05 01:24:39 +0200

    [Feat:Dashboard] Add nav, overview and navside components

---
+++ src/components/dashboard-page/header.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h3>Dashboard Admin</h3>
+    <div class='menu-burger'></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Header',
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ src/components/dashboard-page/navside.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul>
+    <li v-for='link in links'><router-link :to="{name: link.linkPath}">{{link.title}}</router-link></li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    name: 'Navside',
+    data () {
+      return {
+        items: [
+          {
+            title: 'Overview',
+            linkPath: 'dashboard.overview'
+          },
+          {
+            title: 'Create an article',
+            linkPath: 'dashboard.article'
+          },
+          {
+            title: 'Articles list',
+            linkPath: 'dashboard.articles'
+          },
+          {
+            title: 'Presets list',
+            linkPath: 'dashboard.presets'
+          }
+        ]
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
