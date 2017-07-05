1df573b99466a82476f4fea6394cadf6e23f414a
GoyonStephane <stephane.goyon@gmail.com>
2017-07-05 10:49:53 +0200

    [ADD] Generics styles

--- src/App.vue
+++ src/App.vue
@@ -8,65 +8,67 @@
 </template>
 
 <script>
-/* Import components*/
-import navigationList from './components/navigation/list.vue'
-import Vuex from 'vuex'
+  /* Import components*/
+  import navigationList from './components/navigation/list.vue'
+  import Vuex from 'vuex'
 
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  export default {
+    name: 'app',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    },
+    components: {
+      'navigation': navigationList,
+    },
+    computed: {
+      ...Vuex.mapGetters({
+        'isLoggedIn': ''
+      })
     }
-  },
-  components: {
-    'navigation': navigationList,
-  },
-  computed: {
-    ...Vuex.mapGetters({
-      'isLoggedIn': ''
-    })
   }
-}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import 'assets/stylesheet/generics/fonts.scss';
+  @import 'assets/stylesheet/generics/variable.scss';
+  @import 'assets/stylesheet/generics/mixin.scss';
+  @import 'assets/stylesheet/generics/text.scss';
 
-h1, h2 {
-  font-weight: normal;
-}
+  #app {
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 
-a {
-  color: #42b983;
-}
-.transition-page-enter-active {
-  transition: all .3s ease .4s;
-}
-.transition-page-leave-active {
-  transition: all .4s ease;
-}
-.transition-page-enter {
-  opacity: 0;
-}
-.transition-page-leave-to{
-  opacity: 0;
-}
+  a {
+    color: $turquoise;
+  }
+
+  .transition-page-enter-active {
+    transition: all .3s ease .4s;
+  }
+
+  .transition-page-leave-active {
+    transition: all .4s ease;
+  }
+
+  .transition-page-enter {
+    opacity: 0;
+  }
+
+  .transition-page-leave-to {
+    opacity: 0;
+  }
 </style>
