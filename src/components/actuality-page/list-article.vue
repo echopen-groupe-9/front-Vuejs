a5b3529efd198b8af3ed261a7a2ab76225b2df40
tanmba <tan-mba.takougnadi@hetic.net>
2017-07-05 09:31:11 +0200

    liste articles

---
+++ src/components/actuality-page/list-article.vue
@@ -0,0 +1,26 @@
+<template>
+    <ul id="listArticle">
+        <Article></Article>
+        <Article></Article>
+        <Article></Article>
+    </ul>
+</template>
+
+<script>
+    import Article from './article.vue'
+
+    export default {
+        name: 'listArticle',
+        data () {
+            return {
+            }
+        },
+        components: {
+            Article
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>
