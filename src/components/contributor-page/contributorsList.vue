8cba8692df5e1ee3c4bb6d3aba81cb24eb7c17af
Arnvvd <pinot.arnaud@gmail.com>
2017-07-05 23:04:11 +0200

    [Feat:Contributors] - Add Responsive

--- src/components/contributor-page/contributorsList.vue
+++ src/components/contributor-page/contributorsList.vue
@@ -7,32 +7,32 @@
                 <li class="cards-list-item">
                     <router-link class="cards-list-link" :to="{name: 'Contributor.Details', params: {contributor: 'brainer'}}">
                         <h2 class="title-contributor">Brainer</h2>
-                        <p class="text-contributor">Risus Vulputate</p>
-                        <p class="cards-content">Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies ut id elit. Nullam id dolor id nibh.</p>
+                        <p class="cards-subtitle text-contributor">Risus Vulputate</p>
+                        <p class="cards-content text-small">Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies ut id elit. Nullam id dolor id nibh.</p>
                         <span class="cards-background">&nbsp;</span>
                     </router-link>
                 </li>
                 <li class="cards-list-item">
                     <router-link class="cards-list-link" :to="{name: 'Contributor.Details', params: {contributor: 'builder'}}">
                         <h2 class="title-contributor">Builder</h2>
-                        <p class="text-contributor">Risus Vulputate</p>
-                        <p class="cards-content">Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies ut id elit. Nullam id dolor id nibh.</p>
+                        <p class="cards-subtitle text-contributor">Risus Vulputate</p>
+                        <p class="cards-content text-small">Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies ut id elit. Nullam id dolor id nibh.</p>
                         <span class="cards-background">&nbsp;</span>
                     </router-link>
                 </li>
                 <li class="cards-list-item">
                     <router-link class="cards-list-link" :to="{name: 'Contributor.Details', params: {contributor: 'maker'}}">
                         <h2 class="title-contributor">Maker</h2>
-                        <p class="text-contributor">Risus Vulputate</p>
-                        <p class="cards-content">Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies ut id elit. Nullam id dolor id nibh.</p>
+                        <p class="cards-subtitle text-contributor">Risus Vulputate</p>
+                        <p class="cards-content text-small">Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies ut id elit. Nullam id dolor id nibh.</p>
                         <span class="cards-background">&nbsp;</span>
                     </router-link>
                 </li>
                 <li class="cards-list-item">
                     <router-link class="cards-list-link" :to="{name: 'Contributor.Details', params: {contributor: 'linker'}}">
                         <h2 class="title-contributor">Linker</h2>
-                        <p class="text-contributor">Risus Vulputate</p>
-                        <p class="cards-content">Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies ut id elit. Nullam id dolor id nibh.</p>
+                        <p class="cards-subtitle text-contributor">Risus Vulputate</p>
+                        <p class="cards-content text-small">Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies ut id elit. Nullam id dolor id nibh.</p>
                         <span class="cards-background">&nbsp;</span>
                     </router-link>
                 </li>
@@ -50,23 +50,21 @@
     @import '../../core.scss';
 
     .cards-list{
+
         position: relative;
         display: block;
-        height: 450px;
         max-width: 960px;
-        margin: 0 auto;
+        margin: 0 auto 50px;
+
+        @include mq($from: 'desktop'){
+            height: 450px;
+        }
     }
 
     .cards-list-item{
-
-        position: absolute;
-        top: 0;
-        display: inline-block;
-        vertical-align: top;
-        width: 270px;
-        height: 360px;
+        position: relative;
         padding: 40px 30px;
-        margin: 0;
+        margin-bottom: 10px;
         z-index: 0;
 
         &:before{
@@ -82,34 +80,7 @@
             transition: opacity .4s ease-out;
          }
 
-         &:after{
-            content:'';
-              position: absolute;
-              bottom: 0;
-              left: 0;
-              width: 100%;
-              height: 100%;
-              background-size: contain;
-              background-repeat: no-repeat;
-              background-position: bottom;
-              z-index: -2;
-          }
-
-        &:hover{
-            &:before{
-                 opacity: 1;
-                transition: opacity .4s ease-out;
-             }
-
-            .cards-content{
-                opacity: 1;
-                transform: translateY(0);
-                transition: opacity .4s ease-out, transform .4s ease-out;
-            }
-         }
-
         &:nth-child(1){
-             left: 0;
              background-color: #00ADFE;
             &:after{
                  background-image: url("../../assets/images/brainer_card_bg.png")
@@ -117,8 +88,6 @@
         }
 
         &:nth-child(2){
-             left: 24%;
-             margin-top: 30px;
              background-color: #4E76F2;
             &:after{
                  background-image: url("../../assets/images/builder_card_bg.png")
@@ -126,8 +95,6 @@
         }
 
         &:nth-child(3){
-             right: 24%;
-             margin-top: 60px;
              background-color: #444CB2;
             &:after{
                  background-image: url("../../assets/images/maker_card_bg.png")
@@ -135,19 +102,97 @@
         }
 
         &:nth-child(4){
-             right: 0;
-             margin-top: 90px;
              background-color: #3740B0;
             &:after{
                  background-image: url("../../assets/images/linker_card_bg.png");
              }
         }
+
+
+        @include mq($from: 'mobile'){
+            display: inline-block;
+            vertical-align: top;
+            width: calc(50% - 10px);
+            text-align: center;
+
+            &:after{
+                 content:'';
+                 position: absolute;
+                 bottom: 0;
+                 left: 0;
+                 width: 100%;
+                 height: 100%;
+                 background-size: contain;
+                 background-repeat: no-repeat;
+                 background-position: bottom;
+                 z-index: -2;
+             }
+
+            &:nth-child(odd){
+                 margin-right: 5px;
+             }
+            &:nth-child(even){
+                 margin-left: 5px;
+             }
+
+            &:hover{
+                &:before{
+                     opacity: 1;
+                     transition: opacity .4s ease-out;
+                 }
+            }
+        }
+
+        @include mq($from: 'desktop'){
+            position: absolute;
+            top: 0;
+            width: 270px;
+            height: 360px;
+
+            &:nth-child(odd){
+                 margin-right: 0;
+             }
+            &:nth-child(even){
+                 margin-left: 0;
+             }
+
+            &:nth-child(1){
+                left: 0;
+            }
+            &:nth-child(2){
+                 left: 24%;
+                 margin-top: 30px;
+             }
+            &:nth-child(3){
+                 right: 24%;
+                 margin-top: 60px;
+            }
+            &:nth-child(4){
+                 right: 0;
+                 margin-top: 90px;
+            }
+
+            &:hover{
+
+                .cards-content{
+                    opacity: 1;
+                    transform: translateY(0);
+                    transition: opacity .4s ease-out, transform .4s ease-out;
+                }
+            }
+        }
     }
 
-    .cards-content{
-        opacity: 0;
-        transform: translateY(20px);
-        transition: opacity .3s ease-out, transform .3s ease-out;
+    @include mq($from: 'desktop'){
+        .cards-content{
+            opacity: 0;
+            transform: translateY(20px);
+            transition: opacity .3s ease-out, transform .3s ease-out;
+        }
+    }
+
+    .cards-subtitle{
+        padding-bottom: 20px;
     }
 
     .cards-list-link{
