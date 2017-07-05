cd99702498d50817a4a15fff570ad58927e563f2
renanbronchart <renan.bronchart@gmail.com>
2017-07-05 02:33:09 +0200

    [Feat:dashboard] Add routes children on dashboard

--- src/config/routes.js
+++ src/config/routes.js
@@ -2,7 +2,6 @@ import {requireAuth} from '../utils/authentication'
 
 const Home = r => require.ensure([], () => r(require('../components/home-page/index.vue')));
 const Contributor = r => require.ensure([], () => r(require('../components/contributor-page/index.vue')));
-const ContributorDetails = r => require.ensure([], () => r(require('../components/contributor-page/contributorDetails.vue')));
 const Authentication = r => require.ensure([], () => r(require('../components/authentication-page/index.vue')));
 const PageNotFound = r => require.ensure([], () => r(require('../components/404-page/index.vue')));
 
@@ -13,45 +12,38 @@ const Article = r => require.ensure([], () => r(require('../components/dashboard
 const Preset = r => require.ensure([], () => r(require('../components/dashboard-page/preset.vue')));
 
 const routes = [
-  { name: 'Home', path: '/', component: Home},
-  { name: 'Contributor', path: '/contributor', component: Contributor, children: [
+    { name: 'Home', path: '/', component: Home},
+    { name: 'Contributor', path: '/contributor/:contributor', component: Contributor },
+    { name: 'Connexion', path: '/connexion', component: Authentication},
     {
-      name: 'Contributor.Details',
-      path: '/contributor/:contributor',
-      component: ContributorDetails
-    }
-  ]
-  },
-  { name: 'Connexion', path: '/connexion', component: Authentication},
-  {
-    name: 'Dashboard',
-    path: '/dashboard',
-    component: Dashboard,
-    // beforeEnter: requireAuth,
-    children: [
-      {
-        name: 'Dashboard.Overview',
-        path: '',
-        component: Overview
-      },
-      {
-        name: 'Dashboard.Preset',
-        path: '/preset',
-        component: Preset
-      },
-      {
-        name: 'Dashboard.Articles',
-        path: '/articles',
-        component: Articles
-      },
-      {
-        name: 'Dashboard.Article',
-        path: '/article/:id',
-        component: Article
-      },
-    ]
-  },
-  { path: '*', component: PageNotFound }
+      name: 'Dashboard',
+      path: '/dashboard',
+      component: Dashboard,
+      // beforeEnter: requireAuth,
+      children: [
+        {
+          name: 'Dashboard.Overview',
+          path: '',
+          component: Overview
+        },
+        {
+          name: 'Dashboard.Preset',
+          path: '/preset',
+          component: Preset
+        },
+        {
+          name: 'Dashboard.Articles',
+          path: '/articles',
+          component: Articles
+        },
+        {
+          name: 'Dashboard.Article',
+          path: '/article/:id',
+          component: Article
+        },
+      ]
+    },
+    { path: '*', component: PageNotFound }
 ];
 
 export default routes
