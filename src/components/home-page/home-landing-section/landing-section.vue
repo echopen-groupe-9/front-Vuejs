5e14641a8f5e386822efa29cbaeeb3f0e2e4fcd6
Trapdafifou <thibaut.marsal@gmail.com>
2017-07-05 16:14:42 +0200

    [ADD:components/homepage] html for components

---
+++ src/components/home-page/home-landing-section/landing-section.vue
@@ -0,0 +1,38 @@
+<template>
+    <section class="Home-presentation">
+        <div class="Home-presentation_holder">
+            <h1 class="Home-presentation_title" title="together">{{title}}</h1>
+            <p class="Home-presentation_desc"> {{desc}}</p>
+            <a href="/#" class="app-link Home-presentation_link"></a>
+        </div>
+        <div class="Home-presentation_holder">
+            <img src="http://lorempixel.com/400/200/technics" alt="voici le stetographe"
+                 class="Home-presentation_image">
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'home',
+        data () {
+            return {
+                title: 'Let’s make something good',
+                desc: 'Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies ut id elit.'
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+    .Home-presentation_title {
+        position: relative;
+        color: #32325D;
+        font-size: 64px;
+        &:after {
+            content: attr(title);
+            display: block;
+            font-weight: bold;
+        }
+    }
+</style>
---
+++ src/components/home-page/home-review-section/review-section.vue
@@ -0,0 +1,45 @@
+<template>
+    <section class="Home-review">
+        <div class="Home-review_carousel">
+            <div class="Home-review_holder"  v-for="review in reviews">
+                <div class="Home-review_text">
+                    <h2 class="Home-review_title">{{review.title}}</h2>
+                    <p class="Home-review_desc">{{review.desc}}<span class="Home-review_desc--tiny"></span></p>
+                    <div class="Home-review--author"></div>
+                </div>
+                <div class="Home-review_holder--image">
+                    <img src="http://lorempixel.com/400/200/technics" alt="" class="Home-review_image">
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'home',
+        data () {
+            return {
+                reviews: [
+                    {
+                        title: 'Highly Manipulable',
+                        desc: 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec ullamcorper nulla non metus auctor fringilla. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nulla vitae elit libero, a pharetra augue.'
+                    },
+                    {
+                        title: 'Highly Noobish',
+                        desc: 'Vivamus sgittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec ullamcorper nulla non metus auctor fringilla. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nulla vitae elit libero, a pharetra augue.'
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+
+    .Home-review_holder {
+        margin: 0 5vw;
+    }
+
+
+</style>
