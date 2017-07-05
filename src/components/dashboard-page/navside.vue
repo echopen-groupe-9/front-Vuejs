5aee2299a5e9ef326dd18151edb4bd078c757bd3
renanbronchart <renan.bronchart@gmail.com>
2017-07-05 02:42:15 +0200

    [Fix:router-link] Change router link on dashboard

--- src/components/dashboard-page/navside.vue
+++ src/components/dashboard-page/navside.vue
@@ -1,6 +1,9 @@
 <template>
   <ul>
-    <li v-for='link in links'><router-link :to="{name: link.linkPath}">{{link.title}}</router-link></li>
+    <li><router-link :to="{name: 'Dashboard.Overview'}">Overview</router-link></li>
+    <li><router-link :to="{name: 'Dashboard.Article', params: {id: 1}}">Create an article</router-link></li>
+    <li><router-link :to="{name: 'Dashboard.Articles'}">Articles list</router-link></li>
+    <li><router-link :to="{name: 'Dashboard.Preset'}">Presets list</router-link></li>
   </ul>
 </template>
 
@@ -9,24 +12,6 @@
     name: 'Navside',
     data () {
       return {
-        links: [
-          {
-            title: 'Overview',
-            linkPath: 'Dashboard.Overview'
-          },
-          {
-            title: 'Create an article',
-            linkPath: 'Dashboard.Article'
-          },
-          {
-            title: 'Articles list',
-            linkPath: 'Dashboard.Articles'
-          },
-          {
-            title: 'Presets list',
-            linkPath: 'Dashboard.Preset'
-          }
-        ]
       }
     }
   }
