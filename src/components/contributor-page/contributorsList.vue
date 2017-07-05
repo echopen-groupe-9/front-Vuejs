ddf6173e570a6c15870731a9e1d151d378dc2f6e
Arnvvd <pinot.arnaud@gmail.com>
2017-07-05 09:56:58 +0200

    [Feat:Contributors] - Fix routes for contributors details

---
+++ src/components/contributor-page/contributorsList.vue
@@ -0,0 +1,25 @@
+<template>
+    <ul>
+        <li>
+            <router-link :to="{name: 'Contributor.Details', params: {contributor: 'brainer'}}">Brainer</router-link>
+        </li>
+        <li>
+            <router-link :to="{name: 'Contributor.Details', params: {contributor: 'builder'}}">Builder</router-link>
+        </li>
+        <li>
+            <router-link :to="{name: 'Contributor.Details', params: {contributor: 'maker'}}">Maker</router-link>
+        </li>
+        <li>
+            <router-link :to="{name: 'Contributor.Details', params: {contributor: 'linker'}}">Linker</router-link>
+        </li>
+    </ul>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style lang="scss">
+
+</style>
