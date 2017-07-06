740c04432a5c50a5a4adfccecf9fc6176e024c2a
Arnvvd <pinot.arnaud@gmail.com>
2017-07-06 02:29:11 +0200

    [Feat:Actuality] - Prepare active class on scroll

--- src/components/actuality-page/article.vue
+++ src/components/actuality-page/article.vue
@@ -1,16 +1,18 @@
 <template>
-    <li id="article">
-        <h1>Titre</h1>
-        <span>Description</span>
-        <span>Date</span>
-        <span>Auteur</span>
-        <img src="" alt="image">
+    <li class='js-scrollActive' :class="{ 'news-event': news.type == 'event', 'news': news.type == 'news' }" id="article">
+        <h1>Titre : {{news.title}}</h1>
+        <span>Jour : {{news.date}}</span>
+        <span>Auteur : {{news.author}}</span>
+        <span>Contenu : {{news.content}}</span>
+        <span>Url : {{news.url}}</span>
+        <img :src="image" alt="" v-for="image in news.images">
     </li>
 </template>
 
 <script>
     export default {
         name: 'article',
+        props: ['news'],
         data () {
             return {
             }
@@ -18,6 +20,27 @@
     }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+    @import '../../core.scss';
 
+    .news-event{
+        display: block;
+        height: 500px;
+        color: #FFF;
+        background-color: $purple-2;
+        opacity: 0;
+    }
+    
+    .news{
+        display: block;
+        height: 500px;
+        background-color: $turquoise;
+        opacity: 0;
+        transition: opacity .4s ease-in;
+    }
+
+    .is-active{
+        opacity: 1;
+        transition: opacity .4s ease-in;
+    }
 </style>
---
+++ src/datas/news.js
@@ -0,0 +1,56 @@
+const news = {
+  newsData: [
+    {
+      year: '2017',
+      news: [
+        {
+          type: 'event',
+          date: '10.02',
+          author: 'Echopen',
+          title: 'Evénement A',
+          images: [],
+          content: 'Hello world',
+          url: '/'
+        },
+        {
+          type: 'news',
+          date: '10.01',
+          author: 'Echopen',
+          title: 'Actualité de ouf',
+          images: [
+            'dist/assets/images/footer-shape.png'
+          ],
+          content: 'Hello world',
+          url: '/'
+        }
+      ],
+    },
+    {
+      year: '2016',
+      news: [
+        {
+          type: 'news',
+          date: '13.07',
+          author: 'Echopen',
+          title: 'Actualité de ouf',
+          images: [
+            'dist/assets/images/footer-shape.png'
+          ],
+          content: 'Hello world',
+          url: '/'
+        },
+        {
+          type: 'event',
+          date: '12.02',
+          author: 'Echopen',
+          title: 'Event de ouf',
+          images: [],
+          content: 'Hello world',
+          url: '/'
+        }
+      ],
+    },
+  ]
+};
+
+export default news
