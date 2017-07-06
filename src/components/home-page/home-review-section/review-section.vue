10ca9595966332a7c0a1bc82844a5ce249808340
Trapdafifou <thibaut.marsal@gmail.com>
2017-07-06 18:12:14 +0200

    [ADD:components/home-page/review] add slider review

--- src/components/home-page/home-review-section/review-section.vue
+++ src/components/home-page/home-review-section/review-section.vue
@@ -1,9 +1,14 @@
 <template>
     <section class="Home-review main-wrapper">
-        <ul class="Home-review_carousel">
-            <li class="Home-review_holder" v-for="review in reviews">
-                <div class="Home-review_text">
-                    <h2 class="Home-review_title section-subtitle title">Highly <span class="section-subtitle-span">Manipulable</span></h2>
+        <div class="Home-review_carousel">
+            <ul class="Home-review_carousel--list_tabs">
+                <li class="Home-review_carousel--tabs text-2" v-for="(review, index) in reviews" v-on:click="checkIndex(tabsIndex =  index)" :class="tabsIndex === index ? 'is-active' : ''">
+                    {{review.tab}}
+                </li>
+            </ul>
+            <div class="Home-review_holder">
+                <div class="Home-review_text" v-for="(review, index) in reviews" :class="tabsIndex != index ? 'is-out' : 'is-active'">
+                    <h2 class="Home-review_title section-subtitle title">{{review.title}}<span class="section-subtitle-span">{{review.span}}</span></h2>
                     <p class="Home-review_desc text">{{review.desc}}<span class="Home-review_desc--tiny"></span></p>
                     <div class="Home-review--author"></div>
                 </div>
@@ -11,8 +16,8 @@
                     <img src="../../../assets/images/phone-graph.png" alt="" class="Home-review_image phone--image">
                     <img src="../../../assets/images/echograpg-graph.png" alt="" class="Home-review_image echo--image">
                 </div>
-            </li>
-        </ul>
+            </div>
+        </div>
     </section>
 </template>
 
@@ -21,21 +26,38 @@
         name: 'home',
         data () {
             return {
+                tabsIndex: 0,
+                descIndex: 0,
                 reviews: [
                     {
+                        tab: 'Highly Manipulable',
+                        title: 'Highly Manipulable',
                         desc: 'Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec ullamcorper nulla non metus auctor fringilla. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nulla vitae elit libero, a pharetra augue.'
                     },
                     {
+                        tab: 'Highly Noobish',
+                        title: 'Highly Noobish',
                         desc: 'Vivamus sgittis lacus vel augue laoreet rutrum faucibus dolor auctor. Donec ullamcorper nulla non metus auctor fringilla. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nulla vitae elit libero, a pharetra augue.'
                     }
                 ]
             }
         },
-        methods: {}
+        methods: {
+            checkIndex: function (index) {
+                console.log(index)
+                this.tabIndex = this.index
+                this.descIndex = this.index
+            }
+        }
     }
 </script>
 
+
+
+
 <style lang="scss">
+    @import '../../../core.scss';
+
     .Home-review_carousel {
         width:100%;
         margin: 0 auto;
@@ -48,6 +70,7 @@
             align-items: center;
             justify-content: space-around;
             padding: 10px 5%;
+            margin: 0 auto;
 
 
             .Home-review_text {
@@ -55,6 +78,22 @@
                 flex-flow: column wrap;
                 flex: 1;
                 text-align: left;
+
+                &.is-out {
+                    visibility: hidden;
+                    opacity: 0;
+                    transition: visibility 0s, opacity 1s ease;
+                    position: absolute;
+                }
+                &.is-active {
+                    visibility: visible;
+                    opacity: 1;
+                    transition: visibility 0s, opacity 1s ease;
+                }
+
+                .Home-review_title {
+
+                }
             }
             .Home-review_holder--image {
                 flex: 1;
@@ -69,5 +108,45 @@
                 }
             }
         }
+        .Home-review_carousel--tabs {
+            margin: 0 1%;
+            position: relative;
+            transition: ease .3s;
+
+            &:hover {
+                cursor: pointer;
+            }
+            opacity: .8;
+            top: 40px;
+
+            &:after {
+                content: '';
+                position: absolute;
+                background: $blue-2;
+                bottom: -10px;
+                left: calc(50% - 2.5px);
+                display: block;
+                border-radius: 50%;
+                transition: ease .3s;
+                opacity: 0;
+                width: 0;
+                height: 0;
+            }
+
+            &:hover {
+                cursor: pointer;
+            }
+            &.is-active {
+                font-weight: bold;
+
+                &:after {
+                    width: 6px;
+                    height: 6px;
+                    opacity: 1;
+                }
+            }
+        }
     }
 </style>
+
+
