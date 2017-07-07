21d8212e26abeafe842b49b767e4a8a557e877b6
Arnvvd <pinot.arnaud@gmail.com>
2017-07-07 11:32:36 +0200

    [Feat:RedesignHome] - Update contributors datas

--- src/datas/contributors.js
+++ src/datas/contributors.js
@@ -3,23 +3,23 @@ const contributors = {
     {
       type: 'linker',
       content: {
-          baseline: 'Avez-vous l’esprit Linker?',
-          mission: 'Je suis une super mission de linker',
-          role: 'Je suis un super rôle de linker'
-        }
+        baseline: 'Vous aimez rassembler les gens ?',
+        mission: 'Nous aider à tisser des liens forts avec notre communauté',
+        role: "Être linker, c’est rejoindre une communauté, il est l'acteur fondamental de notre organisation et il adhère à un ensemble de valeurs. Ses solutions mises en œuvre, les difficultés techniques rencontrées, ses passions sont primordiales. Pour être un bon linker n’hésitez pas à Coopter et parrainer vos futurs collègues."
+      }
     },
     {
       type: 'builder',
       content: {
         baseline: 'Avez-vous l’esprit D.I.Y?',
         mission: 'Je suis une super mission de Builder',
-        role: 'Je suis un super rôle de Builder'
+        role: "Votre mission est de participer aux développement des logiciels Open Source qui contribuent à la conception opérationnelle de l'appareil médical à ultrasons, au niveau des normes de qualité d'imagerie médicale. L'application mobile pour UX et les performances, le traitement du signal et de l'image, l'analyse des données et IA pour le diagnostic et l'assistance de l'opérateur relève de la mission Build Soft"
       }
     },
     {
       type: 'maker',
       content: {
-      baseline: 'Avez-vous l’esprit D.I.Y?',
+        baseline: 'Avez-vous l’esprit D.I.Y ?',
         mission: 'Nous aider à emmener Echopen toujours plus loin.',
         role: 'Que vous soyez bricolo du dimanche ,  amateur ou militant, si vous avez l’âme d’un Maker et que vous  êtes  motivé par le fait de réaliser avec nous des prototypes qui changeront les pratiques de la médecine aujourd’hui et demain. Vous êtes içi au bon endroit!'
       }
@@ -27,7 +27,7 @@ const contributors = {
     {
       type: 'brainer',
       content: {
-        baseline: 'Avez-vous l’esprit Brainer?',
+        baseline: "Voulez-vous être une tête pense d'Echopen",
         mission: 'Je suis une super mission de brainer',
         role: 'Je suis un super rôle de brainer'
       }
